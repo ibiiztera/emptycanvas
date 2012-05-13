97156c1ce8841f3cd5a859fc879808a257f3b572
DAHMEN Manuel <manuel.dahmen@gmail.com>
2012-05-13 07:36:15 +0200

    test version 3

--- src/be/ibiiztera/md/pmatrix/test/pushmatrix/newtest/TestCollection.java
+++ src/be/ibiiztera/md/pmatrix/test/pushmatrix/newtest/TestCollection.java
@@ -4,10 +4,25 @@
  */
 package be.ibiiztera.md.pmatrix.test.pushmatrix.newtest;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
  * @author Manuel DAHMEN
  */
-public interface TestCollection {
+public class TestCollection {
+    private ArrayList<TestObjet> tests = new ArrayList<TestObjet>();
     
+    public void testCollection()
+    {
+        Iterator<TestObjet> it = tests.iterator();
+        while(it.hasNext())
+            it.next().run();
+    }
+    
+    public void add(TestObjet to)
+    {
+        tests.add(to);
+    }
 }
