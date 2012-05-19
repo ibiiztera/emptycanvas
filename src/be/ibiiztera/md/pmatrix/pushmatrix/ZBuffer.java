a401c5359d2ca024d6ff6e3af7c4eabadecd49fc
DAHMEN Manuel <manuel.dahmen@gmail.com>
2012-05-19 13:30:00 +0200

    interface zbuffer new methode

--- src/be/ibiiztera/md/pmatrix/pushmatrix/ZBuffer.java
+++ src/be/ibiiztera/md/pmatrix/pushmatrix/ZBuffer.java
@@ -7,6 +7,7 @@ public interface ZBuffer
 {
     public ZBuffer getInstance(int x, int y);
     public java.awt.Point coordonneesPointEcran(Point3D p);
+    public java.awt.Point coordonneesPointEcranPerspective(Point3D x3d);
     public int resX();
     public int resY();
     public Scene scene();
@@ -20,11 +21,7 @@ public interface ZBuffer
     public void plotPoint(Point3D p, Color c);
     public void testPoint(Point3D p, Color c);
     public void testPoint(Point3D point3D);
-
     public void suivante();
-
-    public void perspective(double i);
-
     public void isometrique();
-
+    public void perspective(double camera, double planproj);
 }
