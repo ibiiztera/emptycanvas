4de0ea31ebef8484231f3e32dbf581e530c1d205
Manuel DAHMEN <Atelier@192.168.1.4>
2012-06-05 14:26:17 +0200

    couleur et texture: base objets: interface nouvelle définition

---
+++ src/be/ibiiztera/md/pmatrix/pushmatrix/newobjets/BaseObjet.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.ibiiztera.md.pmatrix.pushmatrix.newobjets;
+
+import be.ibiiztera.md.pmatrix.pushmatrix.Matrix33;
+import be.ibiiztera.md.pmatrix.pushmatrix.Point3D;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Atelier
+ */
+public interface BaseObjet {
+    /***
+     * 
+     * @param parametres paramètres de l'objet
+     * @return objet instancié
+     */
+    public BaseObjet instancier(HashMap<String, BaseObjet> parametres);
+    /***
+     * 
+     * @return map de paramètres avec description textuelle
+     */
+    public HashMap<String, String> documentationParametres();
+    /***
+     * 
+     * @param tcolor Couleur ou texture
+     * @return objet
+     */
+    public BaseObjet colorer(TColor tcolor);
+    /***
+     * 
+     * @param position position (Point3D)
+     * @return objet
+     */
+    public BaseObjet positionner(Point3D position);
+    /***
+     * 
+     * @param matriceRotation matrice de rotation
+     * @return objet
+     */
+    public BaseObjet orienter(Matrix33 matriceRotation);
+}
